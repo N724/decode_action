7a8acffe019dd9380e741e7946dbc568792a53cc
action <action@github.com>
2024-06-29 08:12:59 +0000

    Add decoded output file

--- output.py
+++ output.py
@@ -1,25 +1,25 @@
-#2024-06-29 07:57:48
+#2024-06-29 08:12:48
 import json 
 import os 
 import time 
-import brotli 
 import requests 
-def sgin_in(O00O0O000000O0O00,OO00000O0000O00O0):
- O00O0OOO0O00OO00O={"Accept":"*/*","Accept-Encoding":"gzip, deflate, br","Accept-Language":"zh-CN,zh;q=0.9","Connection":"keep-alive","Content-Length":"358","Content-Type":"application/json","Host":"www.kugua.com","Referer":"https://servicewechat.com/wx1f9fc8e79fcbce16/93/page-frame.html","Sec-Fetch-Dest":"empty","Sec-Fetch-Mode":"cors","Sec-Fetch-Site":"cross-site","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 MicroMessenger/7.0.20.1781(0x6700143B) NetType/WIFI MiniProgramEnv/Windows WindowsWechat/WMPF WindowsWechat(0x63090b11)XWEB/9185","xweb_xhr":"1"}
- O0O0OOOO0OO00O0O0={"type":1,"invite_id":"","code_ticket":"","count":"","token":f"{O00O0O000000O0O00}","appid":"wx1f9fc8e79fcbce16","openid":f"{OO00000O0000O00O0}"}
- O00OO00OOOOO00O0O=requests.post('https://www.kugua.com/wxapp/inflatedv3/popUpRedEnvelopes',headers=O00O0OOO0O00OO00O,data=json.dumps(O0O0OOOO0OO00O0O0))
- if O00OO00OOOOO00O0O.status_code==200:
+def sgin_in(OOOOOO00OOOOO0O0O,O0OOO0OO000O000OO):
+ OO00O0O0OO00OOOOO={"Accept":"*/*","Accept-Encoding":"gzip, deflate, br","Accept-Language":"zh-CN,zh;q=0.9","Connection":"keep-alive","Content-Length":"358","Content-Type":"application/json","Host":"www.kugua.com","Referer":"https://servicewechat.com/wx1f9fc8e79fcbce16/93/page-frame.html","Sec-Fetch-Dest":"empty","Sec-Fetch-Mode":"cors","Sec-Fetch-Site":"cross-site","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 MicroMessenger/7.0.20.1781(0x6700143B) NetType/WIFI MiniProgramEnv/Windows WindowsWechat/WMPF WindowsWechat(0x63090b11)XWEB/9185","xweb_xhr":"1"}
+ O0O00O0O000O0O00O={"type":1,"invite_id":"","code_ticket":"","count":"","token":f"{OOOOOO00OOOOO0O0O}","appid":"wx1f9fc8e79fcbce16","openid":f"{O0OOO0OO000O000OO}"}
+ O0000O00000OOOOOO=requests.post('https://www.kugua.com/wxapp/inflatedv3/popUpRedEnvelopes',headers=OO00O0O0OO00OOOOO,data=json.dumps(O0O00O0O000O0O00O))
+ if O0000O00000OOOOOO.status_code==200:
   print("签到成功")
-  O0O00000OOOO00000=O00OO00OOOOO00O0O.content 
-  O0O0OOOOOOOO0O000=json.loads(O0O00000OOOO00000.decode('utf-8'))
-  return O0O0OOOOOOOO0O000["data"]["dialogId"]
+  O0OOOOO00O0O000OO=O0000O00000OOOOOO.content 
+  print(O0OOOOO00O0O000OO.decode("utf-8"))
+  O00OO0O000OO00O00=json.loads(O0OOOOO00O0O000OO.decode('utf-8'))
+  return O00OO0O000OO00O00["data"]["dialogId"]
  else:
-  print(O00OO00OOOOO00O0O.text)
-def 提现(OO0O0OO0O0OO0OO00,O0OOO0O0O000O000O,O0O00O0O0OO00OO00):
- O0OOO0O0O0OOO0O0O={"Accept":"*/*","Accept-Encoding":"gzip, deflate, br","Accept-Language":"zh-CN,zh;q=0.9","Connection":"keep-alive","Content-Length":"324","Content-Type":"application/json","Host":"www.kugua.com","Referer":"https://servicewechat.com/wx1f9fc8e79fcbce16/93/page-frame.html","Sec-Fetch-Dest":"empty","Sec-Fetch-Mode":"cors","Sec-Fetch-Site":"cross-site","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 MicroMessenger/7.0.20.1781(0x6700143B) NetType/WIFI MiniProgramEnv/Windows WindowsWechat/WMPF WindowsWechat(0x6309092b) XWEB/8555","xweb_xhr":"1"}
- O00OOO0OO0OOO0000={"dialogId":int(O0O00O0O0OO00OO00),"token":f"{OO0O0OO0O0OO0OO00}","appid":"wx1f9fc8e79fcbce16","openid":f"{O0OOO0O0O000O000O}"}
- OOO00000OOOO00O0O=requests.post('https://www.kugua.com/wxapp/inflatedv3/receiveRedEnvelopes',headers=O0OOO0O0O0OOO0O0O,data=json.dumps(O00OOO0OO0OOO0000))
- print(OOO00000OOOO00O0O.text)
+  print(O0000O00000OOOOOO.text)
+def 提现(OO00000O0O000OO0O,O00OO0O00O000O0OO,OOO00OO0O0O0OOOO0):
+ O0O0OOO0OO000OOOO={"Accept":"*/*","Accept-Encoding":"gzip, deflate, br","Accept-Language":"zh-CN,zh;q=0.9","Connection":"keep-alive","Content-Length":"324","Content-Type":"application/json","Host":"www.kugua.com","Referer":"https://servicewechat.com/wx1f9fc8e79fcbce16/93/page-frame.html","Sec-Fetch-Dest":"empty","Sec-Fetch-Mode":"cors","Sec-Fetch-Site":"cross-site","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 MicroMessenger/7.0.20.1781(0x6700143B) NetType/WIFI MiniProgramEnv/Windows WindowsWechat/WMPF WindowsWechat(0x6309092b) XWEB/8555","xweb_xhr":"1"}
+ O0OOO0O00O00O0OOO={"dialogId":int(OOO00OO0O0O0OOOO0),"token":f"{OO00000O0O000OO0O}","appid":"wx1f9fc8e79fcbce16","openid":f"{O00OO0O00O000O0OO}"}
+ O00OOOOO00000000O=requests.post('https://www.kugua.com/wxapp/inflatedv3/receiveRedEnvelopes',headers=O0O0OOO0OO000OOOO,data=json.dumps(O0OOO0O00O00O0OOO))
+ print(O00OOOOO00000000O.text)
 def print_ascii_art():
  print("// ┏┓   ┏┓")
  print("// ┏┛┻━━━┛┻┓")
