ff6b8cd44a526d6430b1469bf7ff60e0328761ea
action <action@github.com>
2024-07-13 09:26:19 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Sat Jul 13 2024 08:37:27 GMT+0000 (Coordinated Universal Time)
+//Sat Jul 13 2024 09:26:19 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const _0x503289 = _0x2473fd("电信兑换签到话费"),
