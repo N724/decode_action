5d15ad69aabdf8e5fcdfb87bf63d2a2fc93b95c2
action <action@github.com>
2024-07-18 11:28:26 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Wed Jul 17 2024 12:05:55 GMT+0000 (Coordinated Universal Time)
+//Thu Jul 18 2024 11:28:26 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const _0x38fd23 = new _0x1af518("古茗奶茶抢券"),
