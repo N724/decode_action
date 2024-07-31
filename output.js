7d53db51ac15f574c7bd6accf1e0e6e14b77f7c1
action <action@github.com>
2024-07-31 08:02:23 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Wed Jul 31 2024 07:48:00 GMT+0000 (Coordinated Universal Time)
+//Wed Jul 31 2024 08:02:23 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env('饿了么乐园币');
